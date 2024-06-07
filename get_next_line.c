8b16fa2c49bc5a952b688dabb07ce712060f3fa0
Ruben Facciado <rufaccia@c1r8p3.42nice.fr>
2024-06-07 18:22:02 +0200

    ssasa

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+#include "get_next_line.h"
+size_t read(int fd, void *buf, size_t count)
+{
+	if (read)
+	{
+		/* code */
+	}
+	
+}
+
+char *get_next_line(int fd)
+{
+
+}
+
+
+int	main(void)
+{
+	char buffer[BUFFER_SIZE]
+	int	fd;
+	int	nb_read;
+	int	count;
+	fd = open(/*fichiertxt*/, O_RDONLY);
+	if (fd == -1)
+	count = 0;
+}
---
+++ get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H  
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+#define BUFFER_SIZE 1024
+char *get_next_line(int fd)
+
+
+#endif
